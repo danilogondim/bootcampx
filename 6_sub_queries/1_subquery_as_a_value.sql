480765c3225d8c56157abbce81b6ca1538ad3768
Danilo Gondim <daniloencarnacao@gmail.com>
2020-09-14 23:41:06 +0000

    Add another situation where we can use a subquery as a value

--- 6_sub_queries/1_subquery_as_a_value.sql
+++ 6_sub_queries/1_subquery_as_a_value.sql
@@ -16,3 +16,9 @@ WHERE students.name = 'Ibrahim Schimmel';
 -- FROM assignment_submissions
 -- JOIN students ON students.id = student_id
 -- WHERE students.name = 'Ibrahim Schimmel';
+
+-- or we can use it in comparisons (reference: https://sqlzoo.net/wiki/SELECT_within_SELECT_Tutorial)
+-- SELECT name FROM world
+--   WHERE population >
+--      (SELECT population FROM world
+--       WHERE name='Russia')
