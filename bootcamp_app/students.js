e881fa0d407a86b5bd735df0849ec5082c7edbc7
Danilo Gondim <daniloencarnacao@gmail.com>
2020-09-17 01:25:05 +0000

    Change the query so that it has a join and the cohort is returned by name

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -18,12 +18,12 @@ const pool = new Pool({
 // .catch(err => console.error('query error', err.stack));
 
 pool.query(`
-SELECT id, name, cohort_id
-FROM students
+SELECT students.id as id, students.name as student, cohorts.name as cohort
+FROM students JOIN cohorts ON cohort_id = cohorts.id
 LIMIT 5;
 `)
 .then(res => {
   res.rows.forEach(user => {
-    console.log(`${user.name} has an id of ${user.id} and was in the ${user.cohort_id} cohort`);
+    console.log(`${user.student} has an id of ${user.id} and was in the ${user.cohort} cohort`);
   })
 });
